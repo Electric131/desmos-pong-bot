03c52ddb69411b47ee134b1bb5ac74ee9371e3ac
Electric131 <39623923+Electric131@users.noreply.github.com>
2022-03-18 11:22:53 -0500

    Added Pong Script Detection to lazy.js
    Also removed unnecessary variable

--- lazy.js
+++ lazy.js
@@ -3,7 +3,12 @@
 // Add this to make it hit the ball automatically
 // -- This version is just a lazier version of unstoppable, it does not use a raycast to find where the ball will go, instead just goes to where the ball currently is
 
-var tempPos = {};
+if (typeof gameIn == 'undefined') {
+  alert("Desmos Pong was not detected so it was loaded automatically.");
+  // Get the latest version of desmos pong
+  fetch('https://raw.githubusercontent.com/johndoesstuff/randomProjects/master/desmos%20pong.js').then(data=>data.text().then(function(text){var s=document.createElement("script");s.type ="text/javascript";s.text=text;document.body.appendChild(s);}));
+}
+
 var bot_circularInterval = setInterval(function() {
 	if(inGame && gameIn == 1) {
 		if (gameSpeed >= 99) gotoMenu();
